9251c2635a18c0b6ceacb4e6a966b728fa565303
Hardik Rana <hardikrana276@gmail.com>
2018-02-08 14:06:51 +0530

    Add files via upload

---
+++ Practice/new and delete.cpp
@@ -0,0 +1,25 @@
+//to take marks of different subject from student and calculate sum of that
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,i;
+    float *p,total=0;
+    cout<<"enter total number of subjects\n";
+    cin>>n;
+    //to allocate memory
+    p=new float[n];
+    //p will contain starting address of array
+    cout<<"enter marks\n";
+    for(int i=0;i<n;i++)
+    {
+        cout<<"Subject\n"<<i+1;
+        cin>>*(p+i);//or we can also use cin>>p[i]
+        total=total+*(p+i);
+    }
+    cout<<"total\n"<<total;
+    return 0;
+}
