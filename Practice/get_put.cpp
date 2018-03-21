5e4d1805ab515fc1dbd16b1a64d79e0e114b03d6
Hardik Rana <hardikrana276@gmail.com>
2018-03-21 19:46:05 +0530

    updated difference

--- Practice/get_put.cpp
+++ Practice/get_put.cpp
@@ -14,3 +14,27 @@ int main()
     }
     printf("length of input=%d\n",count1-1);
 }
+
+
+/*
+Run this to know the difference between cin and get() fuunction.
+cin will skip the white space and newline character from input
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int count1=0;
+    char ch;
+    cout<<"enter text"<<endl;
+    while(ch!='\n')
+    {
+        cin>>ch;
+        cout.put(ch);
+        count1++;
+    }
+    printf("length of input=%d\n",count1-1);
+}
+*/
