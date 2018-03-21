4d0cb49c4c060bbbadd802d6de4ee36ffdd99359
Hardik Rana <hardikrana276@gmail.com>
2018-03-21 23:48:12 +0530

    width,precision,fill functions added

---
+++ cpp stream/width_precision_fill.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+
+{
+    for(int i=1;i<10;i++)
+    {
+        cout.width(10);
+
+        cout.fill('*');
+
+        cout.precision(6);
+
+        cout<<sqrt(i)<<endl;
+    }
+}
