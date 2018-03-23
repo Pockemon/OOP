ed50f84a1adea73998bd4bada0cd2e3d7c75530e
Hardik Rana <hardikrana276@gmail.com>
2018-03-23 14:44:46 +0530

    Update width_precision_fill.cpp

--- cpp stream/width_precision_fill.cpp
+++ cpp stream/width_precision_fill.cpp
@@ -9,9 +9,20 @@ int main()
     {
         cout.width(10);
 
+        cout.precision(4);
+
+        cout<<sqrt(i)<<endl;
+    }
+    
+    printf("\n");
+
+    for(int i=1;i<10;i++)
+    {
+        cout.width(10);
+
         cout.fill('*');
 
-        cout.precision(6);
+        cout.precision(4);
 
         cout<<sqrt(i)<<endl;
     }
