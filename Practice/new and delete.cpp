b02ca4be048e069c4fcc2be00a688a391a1d8f9a
Hardik Rana <hardikrana276@gmail.com>
2018-04-13 08:30:31 +0530

    Update new and delete.cpp

--- Practice/new and delete.cpp
+++ Practice/new and delete.cpp
@@ -1,31 +1,39 @@
-/*
-     HARDIK RANA
-     
-    to take marks of different subject using dynamic memory allocation from student and calculate sum of that
- */
-
-#include<iostream>
-
+#include<bits/stdc++.h>
 using namespace std;
+
+class string1
+{
+    char name[20];
+    int age;
+    int salary;
+public:
+    string1()
+    {
+        strcpy(name,"");
+        age=0;
+        salary=0;
+    }
+    void getdata(char *n,int e,int m)
+    {
+        strcpy(name,n);
+        age=e;
+        salary=m;
+    }
+
+    void display(void)
+    {
+        cout<<name<<endl;
+        cout<<age<<endl;
+        cout<<salary<<endl;
+    }
+};
 
 int main()
 {
-    int n,i;
-    float *p,total=0;
-    cout<<"enter total number of subjects\n";
-    cin>>n;
-    //to allocate memory
-    p=new float[n];
-    //p will contain starting address of array
-    cout<<"enter marks\n";
-    for(int i=0;i<n;i++)
-    {
-        cout<<"Subject\n"<<i+1;
-        cin>>*(p+i);//or we can also use cin>>p[i]
-        total=total+*(p+i);
-    }
-    cout<<"total\n"<<total;
-    //cout<<p<<endl;
-    delete []p;
-    return 0;
+    string1 *p;
+    p = new string1;
+    p->getdata("hardik",20,10000);
+    p->display();
+    delete p;
 }
+
