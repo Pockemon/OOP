e4032905fb568aef478adf2d01f74ff94b057254
Hardik Rana <hardikrana276@gmail.com>
2018-04-26 06:59:51 +0530

    singleton class added

---
+++ Practice/singleton.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class st
+{
+private:
+    static st *instance;
+    int data;
+public:
+    static st *Instance()
+    {
+        if(instance==NULL)
+        {
+            instance = new st();
+        }
+        return instance;
+    }
+
+    void setdata(int value)
+    {
+        data=value;
+    }
+    int getdata()
+    {
+        return data;
+    }
+};
+
+st *st::instance = NULL;
+
+int main()
+{
+    st::Instance()->setdata(10);
+
+    cout<<"DATA="<<st::Instance()->getdata()<<endl;
+}
